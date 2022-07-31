6d8d5ee86c026c2759b86c4fa5df4d1988a83513
Feihong Hsu <feihong.hsu@gmail.com>
2022-07-31 13:10:32 -0500

    Close window on key press

--- hello.py
+++ hello.py
@@ -3,9 +3,11 @@ from pyray import *
 init_window(600, 400, "Hello")
 
 while not window_should_close():
+  if is_key_down(KEY_Q):
+    break
   begin_drawing()
   clear_background(WHITE)
-  draw_text("Hello world", 190, 200, 20, VIOLET)
+  draw_text("Hello world", 220, 200, 20, VIOLET)
   end_drawing()
 
 close_window()
