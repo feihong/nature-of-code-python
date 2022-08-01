87844e9bd0cc2cb30ffad123f64cae424a1ee35f
Feihong Hsu <feihong.hsu@gmail.com>
2022-07-31 23:11:28 -0500

    Get rid of flicker in example I.1

--- 0-1-random-walk.py
+++ 0-1-random-walk.py
@@ -7,25 +7,28 @@ class Walker:
   def __init__(self):
     self.x = width // 2
     self.y = height // 2
+    self.points = set()
 
   def display(self):
-    draw_circle(self.x, self.y, 1, VIOLET)
+    for point in self.points:
+      draw_circle_v(point, 1.0, VIOLET)
 
   def step(self):
     stepx = random.randint(-1, 1)
     stepy = random.randint(-1, 1)
     self.x += stepx
     self.y += stepy
+    self.points.add(Vector2(self.x, self.y))
 
 init_window(width, height, 'Traditional random walk')
 set_target_fps(60)
-clear_background(WHITE)
 
 w = Walker()
 
 while not window_should_close():
   begin_drawing()
 
+  clear_background(WHITE)
   w.display()
   w.step()
 
