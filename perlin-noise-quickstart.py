1b04e7e6cb574d4bab6221f18fff7df90a593d2d
Feihong Hsu <feihong.hsu@gmail.com>
2022-08-08 23:04:15 -0500

    Print observed range of perlin noise

--- perlin-noise-quickstart.py
+++ perlin-noise-quickstart.py
@@ -1,9 +1,14 @@
+import functools
+import itertools
 import matplotlib.pyplot as plt
 from perlin_noise import PerlinNoise
 
 noise = PerlinNoise(octaves=10, seed=1)
 xpix, ypix = 100, 100
 pic = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
+
+print('min:', functools.reduce(min, itertools.chain.from_iterable(pic)))
+print('max:', functools.reduce(max, itertools.chain.from_iterable(pic)))
 
 plt.imshow(pic, cmap='gray')
 plt.show()
